877594094f4109593c9eaaa440112958d660d10f
Duy Nguyen <140995090+Duy-Nguyen1104@users.noreply.github.com>
2024-08-14 14:08:25 +0000

    Final version

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
+# Use the latest Ubuntu image as the base
 FROM ubuntu:latest
 
+# Update the package list and install necessary packages
 RUN apt-get update && apt-get install -y \
     python3.10 \
     python3-pip \
-    git
-    
-RUN pip3 install pyYAML
+    git && \
+    pip3 install PyYAML
 
+# Copy the Python script to /usr/bin/
 COPY feed.py /usr/bin/feed.py
 
+# Copy the entrypoint script
 COPY entrypoint.sh /entrypoint.sh
 
+# Give execute permission to the entrypoint script
+RUN chmod +x /entrypoint.sh
+
+# Set the entrypoint to the entrypoint script
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,23 @@
-#!bin/bash
+#!/bin/bash
 
+# Echo a separator line for clarity
 echo "========"
 
-git config --global user.name "${GITHUB_ACTOR}
-git config --global user.name "${INPUT_EMAIL}
+# Configure git with the provided GitHub actor and email
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --add safe.directory /github/workspace
 
+# Run the Python script
 python3 /usr/bin/feed.py
 
+# Add all changes, commit, and push to the main branch
 git add . && git commit -m "Update Feed" 
-git push --set-upstream origin main
+if [ $? -eq 0 ]; then
+    git push origin main
+else
+    echo "No changes to commit"
+fi
 
+# Echo a separator line for clarity
 echo "========"
